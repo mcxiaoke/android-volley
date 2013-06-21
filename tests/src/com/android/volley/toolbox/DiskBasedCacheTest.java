b33d0d6651b0b31e965839211d410136db2dcb5b
Ficus Kirkpatrick <ficus@android.com>
2013-06-21 10:31:12 -0700

    Port CacheHeader away from ObjectOutputStream.
    The standard Java serialization system relies heavily on
    reflection, even for primitive types and Strings, which
    generates a lot of garbage. Move to a simple built-in
    implementation.
    
    Change-Id: I7f35cbfd1963f2aaf726f5b747cef1b2f8a0ad69

--- tests/src/com/android/volley/toolbox/DiskBasedCacheTest.java
+++ tests/src/com/android/volley/toolbox/DiskBasedCacheTest.java
@@ -24,6 +24,7 @@ import com.android.volley.toolbox.DiskBasedCache.CacheHeader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.util.HashMap;
+import java.util.Map;
 
 public class DiskBasedCacheTest extends AndroidTestCase {
 
@@ -52,4 +53,71 @@ public class DiskBasedCacheTest extends AndroidTestCase {
         assertEquals(first.responseHeaders, second.responseHeaders);
     }
 
+    public void testSerializeInt() throws Exception {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DiskBasedCache.writeInt(baos, 0);
+        DiskBasedCache.writeInt(baos, 19791214);
+        DiskBasedCache.writeInt(baos, -20050711);
+        DiskBasedCache.writeInt(baos, Integer.MIN_VALUE);
+        DiskBasedCache.writeInt(baos, Integer.MAX_VALUE);
+        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        assertEquals(DiskBasedCache.readInt(bais), 0);
+        assertEquals(DiskBasedCache.readInt(bais), 19791214);
+        assertEquals(DiskBasedCache.readInt(bais), -20050711);
+        assertEquals(DiskBasedCache.readInt(bais), Integer.MIN_VALUE);
+        assertEquals(DiskBasedCache.readInt(bais), Integer.MAX_VALUE);
+    }
+
+    public void testSerializeLong() throws Exception {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DiskBasedCache.writeLong(baos, 0);
+        DiskBasedCache.writeLong(baos, 31337);
+        DiskBasedCache.writeLong(baos, -4160);
+        DiskBasedCache.writeLong(baos, 4295032832L);
+        DiskBasedCache.writeLong(baos, -4314824046L);
+        DiskBasedCache.writeLong(baos, Long.MIN_VALUE);
+        DiskBasedCache.writeLong(baos, Long.MAX_VALUE);
+        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        assertEquals(DiskBasedCache.readLong(bais), 0);
+        assertEquals(DiskBasedCache.readLong(bais), 31337);
+        assertEquals(DiskBasedCache.readLong(bais), -4160);
+        assertEquals(DiskBasedCache.readLong(bais), 4295032832L);
+        assertEquals(DiskBasedCache.readLong(bais), -4314824046L);
+        assertEquals(DiskBasedCache.readLong(bais), Long.MIN_VALUE);
+        assertEquals(DiskBasedCache.readLong(bais), Long.MAX_VALUE);
+    }
+
+    public void testSerializeString() throws Exception {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DiskBasedCache.writeString(baos, "");
+        DiskBasedCache.writeString(baos, "This is a string.");
+        DiskBasedCache.writeString(baos, "ファイカス");
+        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        assertEquals(DiskBasedCache.readString(bais), "");
+        assertEquals(DiskBasedCache.readString(bais), "This is a string.");
+        assertEquals(DiskBasedCache.readString(bais), "ファイカス");
+    }
+
+    public void testSerializeMap() throws Exception {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        Map<String, String> empty = new HashMap<String, String>();
+        DiskBasedCache.writeStringStringMap(empty, baos);
+        DiskBasedCache.writeStringStringMap(null, baos);
+        Map<String, String> twoThings = new HashMap<String, String>();
+        twoThings.put("first", "thing");
+        twoThings.put("second", "item");
+        DiskBasedCache.writeStringStringMap(twoThings, baos);
+        Map<String, String> emptyKey = new HashMap<String, String>();
+        emptyKey.put("", "value");
+        DiskBasedCache.writeStringStringMap(emptyKey, baos);
+        Map<String, String> emptyValue = new HashMap<String, String>();
+        emptyValue.put("key", "");
+        DiskBasedCache.writeStringStringMap(emptyValue, baos);
+        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        assertEquals(DiskBasedCache.readStringStringMap(bais), empty);
+        assertEquals(DiskBasedCache.readStringStringMap(bais), empty); // null reads back empty
+        assertEquals(DiskBasedCache.readStringStringMap(bais), twoThings);
+        assertEquals(DiskBasedCache.readStringStringMap(bais), emptyKey);
+        assertEquals(DiskBasedCache.readStringStringMap(bais), emptyValue);
+    }
 }
