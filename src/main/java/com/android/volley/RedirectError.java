42f4716e915e2e15217aff7e40cdd233deac7121
mcxiaoke <mcxiaoke@gmail.com>
2015-07-28 10:36:39 +0800

    fix #51 wrong exception for http 301 and 302

---
+++ src/main/java/com/android/volley/RedirectError.java
@@ -0,0 +1,18 @@
+package com.android.volley;
+
+/**
+ * Indicates that there was a redirection.
+ */
+public class RedirectError extends VolleyError {
+
+    public RedirectError() {
+    }
+
+    public RedirectError(final Throwable cause) {
+        super(cause);
+    }
+
+    public RedirectError(final NetworkResponse response) {
+        super(response);
+    }
+}
