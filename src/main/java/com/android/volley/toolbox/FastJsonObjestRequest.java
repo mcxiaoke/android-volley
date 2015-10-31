88e7185ad75733cc5d4acfdc2d0b868605f47fb2
Mr7Cat <flyhanwei@gmail.com>
2015-10-31 13:41:35 +0800

    support fast json lib
    support fast json lib

---
+++ src/main/java/com/android/volley/toolbox/FastJsonObjestRequest.java
@@ -0,0 +1,41 @@
+package com.android.volley.toolbox;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONException;
+import com.alibaba.fastjson.JSONObject;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Response;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Created by hanwei on 2015/10/30.
+ */
+public class FastJsonObjestRequest extends JsonRequest<JSONObject> {
+
+    public FastJsonObjestRequest(int method, String url, String requestBody, Response.Listener<JSONObject> listener, Response.ErrorListener errorListener) {
+        super(method, url, requestBody, listener, errorListener);
+    }
+
+    public FastJsonObjestRequest(int method, String url, JSONObject jsonRequest,
+                                 Response.Listener<JSONObject> listener, Response.ErrorListener errorListener) {
+        super(method, url, (jsonRequest == null) ? null : jsonRequest.toString(), listener,
+                errorListener);
+    }
+
+    @Override
+    protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
+        try {
+            String jsonString = new String(response.data,
+                    HttpHeaderParser.parseCharset(response.headers));
+            JSONObject jsonObject = JSON.parseObject(jsonString);
+            return Response.success(jsonObject,
+                    HttpHeaderParser.parseCacheHeaders(response));
+        } catch (UnsupportedEncodingException e) {
+            return Response.error(new ParseError(e));
+        } catch (JSONException je) {
+            return Response.error(new ParseError(je));
+        }
+    }
+}
